afc2b9088857c84548483545d3c1d057d1c0d5b6
y-asano <yuuki.asano@scsk.jp>
2015-05-28 11:01:15 +0000

    delete duplicate ddl

--- sql/adc/050-foreignkey/aws_ssl_key.sql
+++ sql/adc/050-foreignkey/aws_ssl_key.sql
@@ -1,1 +1,2 @@
-alter table AWS_INSTANCE add constraint AWS_INSTANCE_FK1 foreign key (INSTANCE_NO) references INSTANCE (INSTANCE_NO);
+#alter table AWS_SSL_KEY add constraint AWS_SSL_KEY_FK1 foreign key (FARM_NO) references FARM (FARM_NO);
+#alter table AWS_SSL_KEY add constraint AWS_SSL_KEY_FK2 foreign key (PLATFORM_NO) references PLATFORM (PLATFORM_NO);
