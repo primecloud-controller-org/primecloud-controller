de2b4490244fa18b03550031b1c0413c562b2b45
yterauchi <y.terauchi@scsk.jp>
2015-05-28 16:46:35 +0900

    bugfix client.zip path

--- auto-tool/install-tool/src/main/scripts/createNiftyFiles.sh
+++ auto-tool/install-tool/src/main/scripts/createNiftyFiles.sh
@@ -51,7 +51,7 @@ if [ $? -ne 0 ]; then
     echo "Error: copy failed. jdk."  >> $LOG_FILE 2>&1
     exit 1
 fi
-cp /opt/adc/keys/client.zip ${NIFTYDIR}/niftyimage/openvpn >> $LOG_FILE 2>&1
+cp /etc/openvpn/easy-rsa/keys/client/client.zip ${NIFTYDIR}/niftyimage/openvpn >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: copy failed. client.zip."
     echo "Error: copy failed. client.zip."  >> $LOG_FILE 2>&1
