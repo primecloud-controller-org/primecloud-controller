68548200cd116fc74a1179598ee4455b5a4ca222
ymstmsys <ymstmsys@gmail.com>
2016-10-08 15:25:09 +0900

    Change os from debian to centos of Dockerfile. #76

--- auto-project/auto-zabbix/docker/pcc-zabbix-3.0.4/docker-run.sh
+++ auto-project/auto-zabbix/docker/pcc-zabbix-3.0.4/docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -i -t -p 80:80 -p 3306:3306 --name pcc-zabbix-3.0.4 -d pcc-zabbix:3.0.4
-#docker run --privileged -p 80:80 -p 3306:3306 --name pcc-zabbix-3.0.4 -d pcc-zabbix:3.0.4
-#docker exec -it zabbix /bin/bash
+docker run --privileged -i -t -p 80:80 -p 3306:3306 --name pcc-zabbix-3.0.4 -d pcc-zabbix:3.0.4 /sbin/init
+sleep 5
+docker exec -i -t pcc-zabbix-3.0.4 /entrypoint.sh
---
+++ auto-project/auto-zabbix/docker/pcc-zabbix-3.0.4/docker-terminate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker stop pcc-zabbix-3.0.4
+sleep 1
+docker rm pcc-zabbix-3.0.4
