12964b2354630a187c2989dc92c5deb3c1fa7f42
Masayoshi.Yamashita <masayoshi.yamashita@scsk.jp>
2016-10-21 13:55:39 +0900

    Modify occuring error when api response is empty bean.

--- auto-project/auto-api/src/main/java/jp/primecloud/auto/api/ApiObjectMapperContextResolver.java
+++ auto-project/auto-api/src/main/java/jp/primecloud/auto/api/ApiObjectMapperContextResolver.java
@@ -4,6 +4,7 @@ import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @Provider
@@ -14,6 +15,7 @@ public class ApiObjectMapperContextResolver implements ContextResolver<ObjectMap
     public ApiObjectMapperContextResolver() {
         objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+        objectMapper.disable(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS);
     }
 
     @Override
