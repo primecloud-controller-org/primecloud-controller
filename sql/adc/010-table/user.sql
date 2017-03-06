ade05a9a348ebd2567d9152fc74a94498f47a58a
Masayoshi.Yamashita <masayoshi.yamashita@scsk.jp>
2017-03-06 17:28:33 +0900

    Modify default schema. #94

--- sql/adc/010-table/user.sql
+++ sql/adc/010-table/user.sql
@@ -2,5 +2,6 @@ create table USER (
     USER_NO bigint not null auto_increment,
     USERNAME varchar(50),
     PASSWORD varchar(50),
+    ENABLED boolean not null default true,
     constraint USER_PK primary key(USER_NO)
 );
